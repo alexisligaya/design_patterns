c734f500517ca518e93eb7f6dff562404f6c13b5
Alexis Peters <alexispeters@Alexiss-MacBook-Pro-5.local>
2023-08-30 10:23:01 -0400

    message

---
+++ strategy/Player.java
@@ -0,0 +1,14 @@
+package strategy;
+
+public class Player {
+    private String firstName;
+    private String lastName;
+    private boolean offense;
+
+
+    public Player(String firstName, String lastName, boolean offense){
+
+
+        
+    }
+}
