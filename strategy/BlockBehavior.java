b14dc9af1596c6ea205ada02240aa3162e294b75
Alexis Peters <alexispeters@Alexiss-MacBook-Pro-5.local>
2023-08-30 14:34:20 -0400

    message

---
+++ strategy/BlockBehavior.java
@@ -0,0 +1,11 @@
+package strategy;
+
+public class BlockBehavior {
+    
+    public String play(){
+        
+    }
+
+
+
+}
--- strategy/Player.java
+++ strategy/Player.java
@@ -7,8 +7,22 @@ public class Player {
 
 
     public Player(String firstName, String lastName, boolean offense){
+         
+    }
 
+    public void setDefenseBehavior(){
 
-        
+    }
+
+    public void setOffenseBehavior(){
+
+    }
+
+    public void turnover(){
+
+    }
+
+    public String toString(){
+        return this.firstName + "\t" + this.lastName;
     }
 }
