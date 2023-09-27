f2dee8bf94984d37c62fb340c75b0ed95e152573
alexisligaya <alp12@email.sc.edu>
2023-09-26 22:15:55 -0400

    message

---
+++ singleton/Game.java
@@ -0,0 +1,9 @@
+package singleton;
+import java.util.Random;
+
+public class Game {
+    private Random rand;
+    private Difficulty level;
+    private Anagram currentAnagram;
+    private int getScore;
+}
