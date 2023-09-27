9bb8e1727b7b3c10218c2ff8e46b7d83324eed0f
alexisligaya <alp12@email.sc.edu>
2023-09-27 10:39:31 -0400

    message

--- singleton/Anagram.java
+++ singleton/Anagram.java
@@ -1,5 +1,22 @@
 package singleton;
+import java.util.ArrayList;
 
 public class Anagram {
-    
+ 
+    private String question;
+    private ArrayList<String> answers;
+
+    public Anagram(String question, ArrayList<String> answer){
+        this.question = question;
+        this.answers = new ArrayList<>(answers);
+    }
+
+    public String getQuestion(){
+        return question;
+    }
+
+    public boolean isCorrect(String userAnswer){
+        return answers.contains(userAnswer);
+    }
+
 }
--- singleton/Game.java
+++ singleton/Game.java
@@ -1,9 +1,40 @@
 package singleton;
 import java.util.Random;
+import java.util.HashMap;
 
 public class Game {
+
+    private static Game game;
     private Random rand;
     private Difficulty level;
+    private HashMap<Difficulty, ArrayList<Anagram>> anagrams;
     private Anagram currentAnagram;
-    private int getScore;
+    private int score;
+
+    private Game(){
+        this.rand = new Random();
+        this.level = Difficulty.EASY; //?
+        this.anagrams = new HashMap<>();
+        this.score = 0;
+    }
+
+    public static Game getInstance(){
+        if (instance == null){
+            instance = new Game();
+        }
+        return instance;
+    }
+
+    public String getQuestion(){
+        return question;
+    }
+
+    public boolean isCorrect(String userAnswer){
+        return answers.contains(UserAnswer);
+    }
+
+    public int getScore(){
+        return score;
+    }
+
 }
