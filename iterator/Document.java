c480a9ee6cc61f311b295b13250337c82092f6f1
alexisligaya <alp12@email.sc.edu>
2023-10-04 10:20:43 -0400

    finished assignment

--- iterator/Document.java
+++ iterator/Document.java
@@ -1,24 +1,40 @@
 package iterator;
 import java.util.Stack;
-
+/**
+ * A user can make, undo, and redo changes to a document. 
+ * @author Alexis Peters
+ */
 public class Document {
     private String title;
     private Stack<String> undoStack;
     private Stack<String> redoStack;
 
+    /**
+     * Creates the undo and redo stack
+     * @param title of the document 
+     */
     public Document(String title){
         this.title=title;
         this.undoStack = new Stack<>();
         this.redoStack = new Stack<>();
     }
 
+    /**
+     * Adds changes to the stack
+     * @param change the user makes to the document 
+     * @return displays the changes 
+     */
     public String makeChange(String change){
         undoStack.push(change);
         redoStack.clear();
         return "Making Change: " + change;
-
     }
 
+    /**
+     * User is able to undo changes
+     * @return the changes that are undone
+     * @return null if there are no changes to be undone
+     */
     public String undoChange(){
         if(canUndo()){
             String lastEdit = undoStack.pop();
@@ -28,10 +44,19 @@ public class Document {
         return null;
     }
 
+    /**
+     * User can undo changes 
+     * @return if the stack is not empty
+     */
     public boolean canUndo(){
         return !undoStack.isEmpty();
     }
 
+    /**
+     * User is able to redo changes
+     * @return the changes that are redone
+     * @return null if there are no changes to be redone
+     */
     public String redoChange(){
         if(canRedo()){
             String lastChange = redoStack.pop();
@@ -41,14 +66,26 @@ public class Document {
         return null;
     }
 
+    /**
+     * User can redo changes 
+     * @return if the stack is not empty
+     */
     public boolean canRedo(){
         return !redoStack.isEmpty();
     }
 
+    /**
+     * Updated stack
+     * @return the new stack with changes
+     */
     public StackIterator getUndoIterator(){
         return new StackIterator(undoStack);
     }
-
+    
+    /**
+     * Updated stack
+     * @return the new stack with changes
+     */
     public StackIterator getRedoIterator(){
         return new StackIterator(redoStack);
     }
--- iterator/StackIterator.java
+++ iterator/StackIterator.java
@@ -1,21 +1,36 @@
 package iterator;
 import java.util.Stack;
-
+/**
+ * The stack iterator for a document is implemented.
+ * @author Alexis Peters
+ */
 public class StackIterator {
     private Stack<String> items;
     private int position;
 
+    /**
+     * Creates the stack iterator
+     * @param items the stack with strings of document changes
+     */
     public StackIterator(Stack<String> items){
         this.items = items;
         this.position = 0;
     }
 
+    /**
+     * Checks the next item in the stack
+     * @return the next item or null 
+     */
     public String next(){
         if(hasNext())
             return items.get(position++);
         return null;
     }
 
+    /**
+     * Checks the size of the stack
+     * @return true if the stack has more items
+     */
     public boolean hasNext(){
         return position < items.size();
     }
